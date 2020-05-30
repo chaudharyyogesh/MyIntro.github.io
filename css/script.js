8923dc45a431477e966b1c85c38050d5fa1b7ce7
chaudharyyogesh <chaudharyyogesh84@gmail.com>
2020-05-30 17:29:15 +0545

    updated

---
+++ css/script.js
@@ -0,0 +1,11 @@
+function copyFunc()
+{  
+    var text = document.getElementById("copyEmail").innerText;
+    var elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = text;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+    alert("copied");
+}
